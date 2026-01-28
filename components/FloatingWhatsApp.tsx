263f6956bfc8c58ebff1213fdc9154362878ece2
same7-redaa <samehabdealsalam@gmail.com>
2026-01-28 15:40:22 +0200

    Change floating call button color to green

--- components/FloatingWhatsApp.tsx
+++ components/FloatingWhatsApp.tsx
@@ -14,7 +14,7 @@ const FloatingWhatsApp: React.FC = () => {
       </a>
       <a
         href="tel:+971566636483"
-        className="fixed bottom-6 left-6 w-16 h-16 bg-[#B89564] text-white rounded-none flex items-center justify-center text-2xl shadow-2xl z-[100] hover:scale-110 transition-transform animate-bounce-slow"
+        className="fixed bottom-6 left-6 w-16 h-16 bg-[#25D366] text-white rounded-none flex items-center justify-center text-2xl shadow-2xl z-[100] hover:scale-110 transition-transform animate-bounce-slow"
       >
         <i className="fa-solid fa-phone"></i>
       </a>
